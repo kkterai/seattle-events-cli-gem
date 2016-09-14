d18a81dda781c2985f27ee28e565589a7b94ba9c
Kenlyn Terai <kkterai@gmail.com>
2016-09-14 09:15:54 +0000

    removed commented-out pseudocode

--- lib/seattle_events/events.rb
+++ lib/seattle_events/events.rb
@@ -4,10 +4,6 @@ class SeattleEvents::Events
 
   def self.list
     #It should return a bunch of instances of events
-#    puts <<-DOC.gsub /^\s*/, ''
-#    1. Wed Sep 14 - Learn to Code Seattle - "Hack the Dot Seattle #8: Back to School Edition"
-#    2. Wed Sep 14 - Code Fellows - "Inside Scoop on getting Hired in Tech"
-#    DOC
 
     event_1 = self.new
     event_1.date = "Wed Sep 14"
@@ -15,9 +11,9 @@ class SeattleEvents::Events
     event_1.name = "Hack the Dot Seattle #8: Back to School Edition"
 
     event_2 = self.new
-    event_1.date = "Wed Sep 14"
-    event_1.organization = "Code Fellows"
-    event_1.name = "Inside Scoop on getting Hired in Tech"
+    event_2.date = "Wed Sep 14"
+    event_2.organization = "Code Fellows"
+    event_2.name = "Inside Scoop on getting Hired in Tech"
 
     [event_1, event_2]
   end
