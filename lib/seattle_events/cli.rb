3428680459ba902b5fe0431b15a8db045cbd8208
Kenlyn Terai <kkterai@gmail.com>
2016-09-14 06:17:19 +0000

    begin #5: Stub out the interface

--- lib/seattle_events/cli.rb
+++ lib/seattle_events/cli.rb
@@ -1,9 +1,14 @@
-#CLI Controller
-
 class SeattleEvents::CLI
 
   def call
     puts "Today's Learn-to-Code Event Calendar:"
+    list_events
   end
 
+  def list_events
+    puts <<-DOC
+    1. Wed Sep 14 - Learn to Code Seattle - "Hack the Dot Seattle #8: Back to School Edition"
+    2. Wed Sep 14 - Code Fellows - "Inside Scoop on getting Hired in Tech"
+    DOC
+  end
 end
