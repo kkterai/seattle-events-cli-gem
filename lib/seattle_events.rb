2b006631a43f26f88bd4b62ab27f99f95120e229
kkterai <kkterai@gmail.com>
2016-09-21 10:54:29 -0700

    Revise the gemspec

--- lib/seattle_events.rb
+++ lib/seattle_events.rb
@@ -1,8 +1,8 @@
 #My environment file. Establishes all dependencies.
-require "nokogiri"
-require "open-uri"
-require "pry"
+require_relative './seattle_events/version'
+require_relative './seattle_events/cli'
+require_relative './seattle_events/events'
 
-require_relative "./seattle_events/version"
-require_relative "./seattle_events/cli"
-require_relative "./seattle_events/events"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
